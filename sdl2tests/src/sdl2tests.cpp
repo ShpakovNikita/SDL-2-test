d3ef60c3e59c084a41430cf9c380ee2b75dbed7c
ShpakovNikita <shpakovnikita1998@gmail.com>
2017-11-23 19:48:30 +0200

    Sdl commit test

---
+++ sdl2tests/src/sdl2tests.cpp
@@ -0,0 +1,36 @@
+#include <cstdlib>
+#include <iostream>
+
+#include <SDL2/SDL.h>
+
+std::ostream& operator<<(std::ostream& out, const SDL_version& v) {
+    out << static_cast<int>(v.major) << '.';
+    out << static_cast<int>(v.minor) << '.';
+    out << static_cast<int>(v.patch);
+    return out;
+}
+
+int main(int /*argc*/, char* /*argv*/ []) {
+    SDL_version compiled = {0, 0, 0};
+    SDL_version linked = {0, 0, 0};
+
+    SDL_VERSION(&compiled);
+    SDL_GetVersion(&linked);
+
+    if (SDL_COMPILEDVERSION !=
+        SDL_VERSIONNUM(linked.major, linked.minor, linked.patch)) {
+        std::cerr << "warning: SDL2 compiled and linked version mismatch: "
+                  << compiled << " " << linked << std::endl;
+    }
+
+    const int init_result = SDL_Init(SDL_INIT_EVERYTHING);
+    if (init_result != 0) {
+        const char* err_message = SDL_GetError();
+        std::cerr << "error: failed call SDL_Init: " << err_message
+                  << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "Yes!" << std::endl;
+    return EXIT_SUCCESS;
+}
