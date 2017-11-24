8535f95e4fa8f2c73af4d3b62593e4f9c43532d1
ShpakovNikita <shpakovnikita1998@gmail.com>
2017-11-24 09:17:27 +0200

    Try to commit from eclipse

--- sdl2tests/src/sdl2tests.cpp
+++ sdl2tests/src/sdl2tests.cpp
@@ -31,6 +31,9 @@ int main(int /*argc*/, char* /*argv*/ []) {
         return EXIT_FAILURE;
     }
 
-    std::cout << "Yes!" << std::endl;
+    SDL_Window* const window =
+        SDL_CreateWindow("Chlorine-5", SDL_WINDOWPOS_CENTERED,
+                         SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+
     return EXIT_SUCCESS;
 }
