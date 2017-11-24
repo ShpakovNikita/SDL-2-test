53cb8e6c066101ef7db8bebcce025bfac20e3a4f
ShpakovNikita <shpakovnikita1998@gmail.com>
2017-11-24 23:04:00 +0200

    Test

--- sdl2tests/src/sdl2tests.cpp
+++ sdl2tests/src/sdl2tests.cpp
@@ -9,6 +9,7 @@ std::ostream& operator<<(std::ostream& out, const SDL_version& v) {
     out << static_cast<int>(v.major) << '.';
     out << static_cast<int>(v.minor) << '.';
     out << static_cast<int>(v.patch);
+
     return out;
 }
 
