cb97657d87de54e6d5fe4e0ed4c4540ef67d9f89
ShpakovNikita <shpakovnikita1998@gmail.com>
2017-12-01 22:44:06 +0200

    And again final

--- sdl2-triangle/src/engine.cxx
+++ sdl2-triangle/src/engine.cxx
@@ -23,21 +23,6 @@
 
 #include <windows.h>
 
-const GLchar* vertexShaderSource =
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 position;\n"
-    "void main()\n"
-    "{\n"
-    "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
-    "}\0";
-const GLchar* fragmentShaderSource =
-    "#version 330 core\n"
-    "out vec4 color;\n"
-    "void main()\n"
-    "{\n"
-    "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
-
 const std::string FRAGMENT_PATH = "shaders\\simple_fragment.glsl";
 const std::string VERTEX_PATH = "shaders\\simple_vertex.glsl";
 
@@ -312,10 +297,10 @@ class engine_impl final : public engine {
         auto data = convert_triangle(t);
 
         GLuint vertex_shader =
-            compile_shader(vertexShaderSource, GL_VERTEX_SHADER);
+            compile_shader(get_source(VERTEX_PATH).c_str(), GL_VERTEX_SHADER);
 
-        GLuint fragment_shader =
-            compile_shader(fragmentShaderSource, GL_FRAGMENT_SHADER);
+        GLuint fragment_shader = compile_shader(
+            get_source(FRAGMENT_PATH).c_str(), GL_FRAGMENT_SHADER);
 
         GLuint shader_program =
             create_shader_program(vertex_shader, fragment_shader);
